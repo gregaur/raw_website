274c76d7c78b6593fba7773ff4f89976746d46fb
Gregory Rawlinson <gregraw@hotmail.com>
2017-06-12 09:07:43 +1200

    no message

--- app/js/main.js
+++ app/js/main.js
@@ -10,10 +10,16 @@ $(document).ready(function () {
 
 	$(".menu_icon").click(function(){
 		$(".menu_overlay").toggleClass("hidden");
+		if($(".menu_overlay").hasClass("hidden")){
+			$(this).removeClass("close-icon");
+		}else{
+			$(this).addClass("close-icon");
+		}
 	});
 
-	$(".menu_project_heading,.menu_title").click(function(){
+	$(".menu_project_heading a,.menu_title").click(function(){
 		$(".menu_overlay").addClass("hidden");
+		$(".menu_icon").removeClass("close-icon");
 	});
 
 
