d07e7e67dc06f21641eedc88fb91cfb2700e9ea0
Gregory Rawlinson <gregraw@hotmail.com>
2017-06-14 16:36:27 +1200

    no message

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,9 @@
 
 $(document).ready(function () {
 
-// 		$(".scroll_down").click(function() {
-//     $('html, body').animate({
-//         scrollTop: $("#intro").offset().top
-//     }, 1000);
-// });
+	setTimeout(function() {
+        $(".main").removeClass("initialise");
+    },1000);
 
 	$(".menu_icon").click(function(){
 		$(".menu_overlay").toggleClass("hidden");
@@ -22,6 +20,51 @@ $(document).ready(function () {
 		$(".menu_icon").removeClass("close-icon");
 	});
 
+
+
+	//----------------------------------------DEBOUNCE FUNCTION
+
+	// Returns a function, that, as long as it continues to be invoked, will not
+	// be triggered. The function will be called after it stops being called for
+	// N milliseconds. If `immediate` is passed, trigger the function on the
+	// leading edge, instead of the trailing.
+	function debounce(func, wait, immediate) {
+		var timeout;
+		return function() {
+			var context = this, args = arguments;
+			var later = function() {
+				timeout = null;
+				if (!immediate) func.apply(context, args);
+			};
+			var callNow = immediate && !timeout;
+			clearTimeout(timeout);
+			timeout = setTimeout(later, wait);
+			if (callNow) func.apply(context, args);
+		};
+	};
+
+	var myEfficientFn = debounce(function() {
+	// All the taxing stuff you do
+	}, 250);
+
+	window.addEventListener('resize', myEfficientFn);
+
+
+	//----------------------------------------RESIZE AND AT THE END FIRE AN ACTION
+
+	var resizeTimer;
+
+	$(window).on('resize', function(e) {
+
+
+	  clearTimeout(resizeTimer);
+	  resizeTimer = setTimeout(function() {
+
+	    // Run code here, resizing has "stopped"
+	            
+	  }, 250);
+
+	});
 
 
 
